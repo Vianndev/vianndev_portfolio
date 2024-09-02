be06b7c639ae75b28d5a08f77f62f70b456b9732
Vianney <thescraft69@gmail.com>
2024-09-02 12:43:05 +0200

    feat: installed Poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./*.{html,js}","./js/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins-regular': ['Poppins Regular', 'sans-serif'],
+        'poppins-italic': ['Poppins Italic', 'sans-serif'],
+        'poppins-thin': ['Poppins Thin', 'sans-serif'],
+        'poppins-thin-italic': ['Poppins Thin Italic', 'sans-serif'],
+        'poppins-extralight': ['Poppins ExtraLight', 'sans-serif'],
+        'poppins-extralight-italic': ['Poppins ExtraLight Italic', 'sans-serif'],
+        'poppins-light': ['Poppins Light', 'sans-serif'],
+        'poppins-light-italic': ['Poppins Light Italic', 'sans-serif'],
+        'poppins-medium': ['Poppins Medium', 'sans-serif'],
+        'poppins-medium-italic': ['Poppins Medium Italic', 'sans-serif'],
+        'poppins-semibold': ['Poppins SemiBold', 'sans-serif'],
+        'poppins-semibold-italic': ['Poppins SemiBold Italic', 'sans-serif'],
+        'poppins-bold': ['Poppins Bold', 'sans-serif'],
+        'poppins-bold-italic': ['Poppins Bold Italic', 'sans-serif'],
+        'poppins-extrabold': ['Poppins ExtraBold', 'sans-serif'],
+        'poppins-extrabold-italic': ['Poppins ExtraBold Italic', 'sans-serif'],
+        'poppins-black': ['Poppins Black', 'sans-serif'],
+        'poppins-black-italic': ['Poppins Black Italic', 'sans-serif'],
+      },
+    },
   },
   plugins: [
     require('daisyui'),
