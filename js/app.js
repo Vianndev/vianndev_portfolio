eaa36109b345bda91c2a4ad671f1ce138fd8cdd7
Vianney <25510145+Vianndev@users.noreply.github.com>
2024-09-26 15:35:19 +0200

    fix:added better styling to reviews cards

--- js/app.js
+++ js/app.js
@@ -1,21 +1,22 @@
-const is_review_received_checkbox = document.getElementById('is_review_received');
-
-function toggleReviews(checkbox) {
+function toggleReviews(is_review_received_checkbox) {
   const reviewGiven = Array.from(document.getElementsByClassName('review_given'));
   const reviewReceived = Array.from(document.getElementsByClassName('review_received'));
 
-  if (checkbox.checked) {
-    reviewGiven.forEach(element => element.classList.add('hidden'));
-    reviewReceived.forEach(element => element.classList.remove('hidden'));
+  if (is_review_received_checkbox.checked) {
+    reviewGiven.forEach(element => element.classList.add('!hidden'));
+    reviewReceived.forEach(element => element.classList.remove('!hidden'));
     return;
   }
 
-  reviewGiven.forEach(element => element.classList.remove('hidden'));
-  reviewReceived.forEach(element => element.classList.add('hidden'));
+  reviewGiven.forEach(element => element.classList.remove('!hidden'));
+  reviewReceived.forEach(element => element.classList.add('!hidden'));
 }
-
-// Initialize on page load
-toggleReviews(is_review_received_checkbox);
+document.addEventListener("DOMContentLoaded", function() {
+  const is_review_received_checkbox = document.getElementById('is_review_received');
+  // Initialize on page load
+  toggleReviews(is_review_received_checkbox);
 
 // Add event listener for the checkbox
-is_review_received_checkbox.addEventListener('change', () => toggleReviews(is_review_received_checkbox));
+  is_review_received_checkbox.addEventListener('change', () => toggleReviews(is_review_received_checkbox));
+});
+
